c973335f457a928ea2e039be222aa2502385d78f
fivesun1228@163.com <fivesun1228@163.com>
2023-02-02 12:46:02 +0800

    更换vercel服务器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,28 +1,28 @@
-import axios from 'axios'
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://8.130.102.64:3000/'
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL =
+    "https://netease-cloud-music-api-mocha-zeta.vercel.app/";
+axios.defaults.headers.post["Content-Type"] =
+    "application/x-www-form-urlencoded";
+axios.defaults.withCredentials = true;
 
-
-export default function request(url, type = 'GET', data = {}) {
+export default function request(url, type = "GET", data = {}) {
     return new Promise((resolve, reject) => {
         const options = {
             url,
             method: type,
-        }
-        if (type === 'GET') {
-            options.params = data
+        };
+        if (type === "GET") {
+            options.params = data;
         } else {
-            options.data = data
+            options.data = data;
         }
         axios(options)
-            .then(res => {
-                resolve(res)
+            .then((res) => {
+                resolve(res);
             })
-            .catch(res => {
-                reject(res)
-            })
-    })
+            .catch((res) => {
+                reject(res);
+            });
+    });
 }
-
